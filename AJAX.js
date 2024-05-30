3b366e91195adaf1b640d1ea9b020d0d47b06b2d
Manikant Jha <being.manikantjha@gmail.com>
2024-05-30 11:32:45 +0530

    Made changes to AJAX.js

---
+++ AJAX.js
@@ -0,0 +1,25 @@
+AJAX
+Asynchronous JavaScript and XML
+
+AJAX bina page refresh kiye server se communicate karne ka ek tarika hai.
+
+
+
+XML
+<note>
+    <to>Tove</to>
+    <from>Jani</from>
+    <heading>Reminder</heading>
+    <body>Don't forget me this weekend!</body>
+</note>
+
+JSON
+{
+    note: {
+        to: "Tove",
+        from: "Jani",
+        heading: "Reminder",
+        body: "Don't forget me this weekend!"
+    }
+}
+
